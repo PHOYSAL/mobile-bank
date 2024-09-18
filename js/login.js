dff3530ef6999ace004b309587ec418842661e71
PHOYSAL <phoysal0007@gmail.com>
2024-09-18 23:33:43 +0600

    mobile bank home page added

--- js/login.js
+++ js/login.js
@@ -2,8 +2,16 @@
 
 document.getElementById('btn-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('login button clicked');
+    // console.log('login button clicked');
 
     const mobileNumber=document.getElementById('mobile-number').value;
-    console.log(mobileNumber);
+    const pinNumber=document.getElementById('pin-number').value;
+    // console.log(mobileNumber,pinNumber);
+    if(mobileNumber==='5' && pinNumber==='1234'){
+        // console.log('You are logged in');
+        window.location.href='/home.html'
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
 })
